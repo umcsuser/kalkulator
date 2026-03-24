5bdc707d7582efae45e8090cace994d2b3a568e0
umcsuser <kwasniewiczl@office.umcs.pl>
2026-03-24 17:16:00 +0100

    bez add:)

--- src/main/java/org/example/kalkualtor/Main.java
+++ src/main/java/org/example/kalkualtor/Main.java
@@ -9,4 +9,7 @@ public class Main {
     public static double add(double a, double b) {
         return a + b;
     }
+    public static double add2(double a, double b) {
+        return a + b;
+    }
 }
